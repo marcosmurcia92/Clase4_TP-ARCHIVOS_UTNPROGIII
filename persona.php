c345a5bb69c79a4539c4c8300da9fa1c6641b9c0
Marcos Facundo Murcia <marcosmurcia92@gmail.com>
2016-04-07 21:41:30 -0300

    Clase Persona Maquetada

---
+++ persona.php
@@ -0,0 +1,42 @@
+<?php 
+	abstract class Persona{
+		private $_apellido = '';
+		private $_dni = 0;
+		private $_nombre = '';
+		private $_sexo = '';
+
+		function __construct($nombre,$apellido,$dni,$sexo){
+			$this->_nombre = $nombre;
+			$this->_dni = $dni;
+			$this->_apellido = $apellido;
+			$this->_sexo = $sexo;
+		}
+
+		function getApellido(){
+			return $_apellido;
+		}
+
+		function getDni(){
+			return $_dni;
+		}
+
+		function getNombre(){
+			return $_nombre;
+		}
+
+		function getSexo(){
+			return $_sexo;
+		}
+
+		function Hablar($idioma){
+			return "";
+		}
+
+		function ToString(){
+			return "Nombre: ".$_nombre."<br>".
+					"Apellido: ".$_apellido."<br>".
+					"Dni: ".$_dni." | Sexo: ".$_sexo;
+		}
+	}
+
+ ?>
